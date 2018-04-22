8cb1bbc434480888660778e068053bb0f0f6a12f
Noah <noahjacobs@berkeley.edu>
2018-04-21 23:40:18 -0700

    isolated particle system:

--- js/index3.js
+++ js/index3.js
@@ -326,13 +326,13 @@ function createObjects() {
     } );
     scene.add( particleSystem );
     options = {
-        position: new THREE.Vector3(),
+        position: new THREE.Vector3(10,10,10),
         positionRandomness: .3,
         velocity: new THREE.Vector3(),
         velocityRandomness: .5,
         color: 0xaa88ff,
         colorRandomness: .2,
-        turbulence: .5,
+        turbulence: 0,
         lifetime: 2,
         size: 5,
         sizeRandomness: 1
@@ -620,9 +620,9 @@ function render() {
     tick += delta;
     if ( tick < 0 ) tick = 0;
     if ( delta > 0 ) {
-        options.position.x = Math.sin( tick * spawnerOptions.horizontalSpeed ) * 20;
-        options.position.y = Math.sin( tick * spawnerOptions.verticalSpeed ) * 10;
-        options.position.z = Math.sin( tick * spawnerOptions.horizontalSpeed + spawnerOptions.verticalSpeed ) * 5;
+        // options.position.x = Math.sin( tick * spawnerOptions.horizontalSpeed ) * 20;
+        // options.position.y = Math.sin( tick * spawnerOptions.verticalSpeed ) * 10;
+        // options.position.z = Math.sin( tick * spawnerOptions.horizontalSpeed + spawnerOptions.verticalSpeed ) * 5;
         for ( var x = 0; x < spawnerOptions.spawnRate * delta; x++ ) {
             // Yep, that's really it.   Spawning particles is super cheap, and once you spawn them, the rest of
             // their lifecycle is handled entirely on the GPU, driven by a time uniform updated below
